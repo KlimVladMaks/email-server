d6e34294bceb58ebf5ca154978afdc4dd008806e
vlad <vkm.blog@yandex.ru>
2026-01-07 20:04:33 +0300

    Commit 5

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from .database import Base
+
+class User(Base):
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
