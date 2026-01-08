bb8bb295e72c15956f3eba348aadbda1b59dd790
vlad <vkm.blog@yandex.ru>
2026-01-08 23:18:19 +0300

    Commit 11

--- app/schemas.py
+++ app/schemas.py
@@ -4,6 +4,7 @@ class UserCreate(BaseModel):
     username: str
     password: str
 
+
 class UserResponse(BaseModel):
     id: int
     username: str
--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,4 @@
 from passlib.context import CryptContext
-from jose import jwt, JWTError
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -11,20 +10,3 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
-
-def create_access_token(data: dict):
-    """
-    Создание JWT токена
-    """
-    encoded_jwt = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-def verify_token(token: str):
-    """
-    Верификация JWT токена
-    """
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        return payload
-    except JWTError:
-        return None
