5f92b2dcba65dd7ca7019116691ec980811ffdb3
vlad <vkm.blog@yandex.ru>
2026-01-27 23:24:06 +0300

    Commit 30

--- tests/tests.py
+++ tests/tests.py
@@ -1,23 +1,9 @@
 import requests
-import subprocess
-import time
 from .server import Server
 
 BASE_URL = "http://127.0.0.1:8000"
 
 class Test_1:
-    # def start_server(self):
-    #     """Запуск сервера"""
-    #     print("Запуск сервера FastAPI...")
-    #     server_process = subprocess.Popen(
-    #         ["fastapi", "dev", "app/main.py"],
-    #         stdout=subprocess.DEVNULL,
-    #         stderr=subprocess.DEVNULL
-    #     )
-    #     print("Ожидание запуска сервера (5 секунд)...")
-    #     time.sleep(5)
-    #     return server_process
-
     def make_request(self, method: str, endpoint: str, **kwargs):
         """Универсальный метод для выполнения запросов"""
         url = f"{self.base_url}{endpoint}"
